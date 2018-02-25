2f4812d7c5b9b762c7ac51a5c1b175cef9f7ed7b
SubhamRajgaria <subham.rajgaria@gmail.com>
2018-02-25 18:03:55 +0530

    Added files

--- TCPServer/tcpP2P.c
+++ TCPServer/tcpP2P.c
@@ -71,9 +71,8 @@ typedef struct
 }user_info;
 
 user_info users[5];
-users[0].name = (char *)"subham";
-users[0].ip = (char *)"10.147.115.60";
-users[0].port_no = 8085;
+
+
 
 
 
@@ -82,10 +81,30 @@ void error(char *msg) {
   exit(1);
 }
 int main(int argc, char **argv) {
+  users[0].name = (char *)"subham";
+	users[0].ip = (char *)"10.145.170.183";
+	users[0].port_no = 8085;
+
+	users[1].name = (char *)"localhost";
+	users[1].ip = (char *)"127.0.0.1";
+	users[1].port_no = 8086;
+
+	users[2].name = (char *)"server";
+	users[2].ip = (char *)"10.5.18.69";
+	users[2].port_no = 8086;
+
+	users[3].name = (char *)"praj";
+	users[3].ip = (char *)"10.145.194.229";
+	users[3].port_no = 8085;
+
+	users[4].name = (char *)"avinab";
+	users[4].ip = (char *)"10.145.207.10";
+	users[4].port_no = 8086;
+
   int parentfd; /* parent socket */
   int childfd; /* child socket */
   int portno; /* port to listen on */
-  int clientlen; /* byte size of client's address */
+  unsigned int clientlen; /* byte size of client's address */
   struct sockaddr_in serveraddr,serveraddr1; /* server's addr */
   struct sockaddr_in clientaddr; /* client addr */
   struct hostent *hostp; /* client host info */
@@ -94,7 +113,7 @@ int main(int argc, char **argv) {
   int optval; /* flag value for setsockopt */
   int n; /* message byte size */
   fileDetails f;
-  fd_set master;
+  fd_set master, read_fds;
   int i;
   int client_socket[5]={0};
   /* add stdin and the sock fd to master fd_set */
@@ -156,17 +175,24 @@ int main(int argc, char **argv) {
    * then close connection.
    */
   clientlen = sizeof(clientaddr);
+  FD_ZERO(&master);
+  FD_ZERO(&read_fds);
+  FD_SET(parentfd, &master);
+  FD_SET(STDIN_FILENO,&master);
+
   int max_fd,fd;
+  max_fd = parentfd;
   while(1)
   {
     struct timeval t;
-    t.tv_sec = 100;
+    t.tv_sec = 10;
     t.tv_usec =0;
-    FD_ZERO(&master);
+    
     //add master socket to set
-    FD_SET(parentfd, &master);
-    FD_SET(STDIN_FILENO,&master);
-    max_fd = parentfd;
+    
+    
+    
+    read_fds = master;
     //add child sockets to set
     for ( i = 0 ; i < MAXCLIENTS ; i++) 
     {
@@ -180,7 +206,8 @@ int main(int argc, char **argv) {
                 max_fd = fd;
     }
     //wait for an activity on one of the sockets , timeout is NULL , so wait indefinitely
-    int activity = select( max_fd + 1 , &master , NULL , NULL ,&t);
+    int activity = select( max_fd + 1 , &read_fds , NULL , NULL ,&t);
+    //printf("Activity %d\n", activity);
     if ((activity < 0) && (errno!=EINTR)) 
     {
             printf("select error");
@@ -188,71 +215,83 @@ int main(int argc, char **argv) {
     for (i= 0; i < MAXCLIENTS; i++) {
         
         fd = client_socket[i];
-        .
-        if (FD_ISSET(fd, &master)) {
+
+        if (FD_ISSET(fd, &read_fds)) {
+        	  printf("Reading %d\n", fd);
             if (fd == STDIN_FILENO) {
-                 
-                 bzero(buf,BUFSIZE);
-                 scanf(" %[^\n]s",buf);
-                 printf("hello\n");
-                 printf("%s\n", buf);
-                 char* hostname = strtok(buf,"/");
-                 char* msg = strtok(NULL,"\n");
-                 int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-                 if (sockfd < 0) 
-                    error("ERROR opening socket");
-                 /* gethostbyname: get the server's DNS entry */
-                /*struct pollfd timer;
-        		timer.fd = sockfd;
-        		timer.events = POLLIN;
-            	ret = poll(&fd, 1, 1000); // 1 second for timeout*/
-            	for(int i=0;i<5;i++){
-            		if(strcmp(users[i].name, hostname)==0){
-            			hostname = users[i].ip;
-            			break;
-            		}
-            	}
-                 struct hostent* server = gethostbyname(hostname);
-                 if (server == NULL) {
-                      fprintf(stderr,"ERROR, no such host as %s\n", hostname);
-                      exit(0);
-                 }
+            	bzero(buf,BUFSIZE);
+            	scanf(" %[^\n]s",buf);
+            	printf("hello\n");
+            	printf("%s\n", buf);
+            	char* hostname = strtok(buf,"/");
+            	char* msg = strtok(NULL,"\n");
+            	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            	if (sockfd < 0) 
+            		error("ERROR opening socket");
+              /* gethostbyname: get the server's DNS entry */
+              char *recv_ip;
+              int recv_port;
+              int j;
+              for(j=0;j<5; j++){
+              	if(strcmp(users[j].name, hostname)==0){
+              		recv_ip = users[j].ip;
+              		recv_port = users[j].port_no;
+              		break;
+              	}
+              }
+              printf("Sending message to %s\n", recv_ip);
+              struct hostent* server = gethostbyname(recv_ip);
+              if (server == NULL) {
+                fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+                exit(0);
+              }
                  /* build the server's Internet address */
-                 bzero((char *) &serveraddr1, sizeof(serveraddr1));
-                 serveraddr1.sin_family = AF_INET;
-                 bcopy((char *)server->h_addr, (char *)&serveraddr1.sin_addr.s_addr, server->h_length);
-                 serveraddr1.sin_port = htons(portno);
-                 /* connect: create a connection with the server */
-                 if (connect(sockfd, &serveraddr1, sizeof(serveraddr1)) < 0) 
-                       error("ERROR connecting");
-                 write(sockfd,msg,strlen(msg));
+              bzero((char *) &serveraddr1, sizeof(serveraddr1));
+              serveraddr1.sin_family = AF_INET;
+              bcopy((char *)server->h_addr, (char *)&serveraddr1.sin_addr.s_addr, server->h_length);
+              serveraddr1.sin_port = htons(recv_port);
+              printf("%d\n", serveraddr1.sin_port);
+                 //printf("%s\n", serveraddr1.sin_addr.s_addr );
+              /* connect: create a connection with the server */
+              if (connect(sockfd, &serveraddr1, sizeof(serveraddr1)) < 0) 
+              	error("ERROR connecting");
+              n = send(sockfd,msg,strlen(msg),0);
+              if (n < 0) 
+      					error("ERROR writing to socket");
+              printf("Socket : %d\n", sockfd);
 
             }
             else if (fd == parentfd) {
-                
-                childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
-                if (childfd < 0) 
-                    error("ERROR on accept");
+            	 printf("Parent %d\n", fd);
+              childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+              if (childfd < 0) 
+              	error("ERROR on accept");
                 //add new socket to array of sockets
-                for (i = 0; i < MAXCLIENTS; i++) 
-                {
-                  //if position is empty
-                  if( client_socket[i] == 0 )
-                  {
-                    client_socket[i] = childfd;
-
-                    printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , childfd , inet_ntoa(clientaddr.sin_addr) , ntohs(clientaddr.sin_port));
-                    printf("Adding to list of sockets as %d\n" , i);
-                    break;
-                  }
-                }
+              int j;
+              for (j = 0; j < MAXCLIENTS; j++) 
+              {
+              	//if position is empty
+              	printf("Client %d\n", childfd);
+              	if( client_socket[j] == 0 )
+              	{
+              		client_socket[j] = childfd;
+              		FD_SET(childfd, &master);
+              		if(childfd>max_fd)
+              			max_fd= childfd;
+              		printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , childfd , inet_ntoa(clientaddr.sin_addr) , ntohs(clientaddr.sin_port));
+              		printf("Adding to list of sockets as %d\n" , j);
+              		break;
+              	}
+              }
             }
             else {
-                
-                int n = read(fd,buf,BUFSIZE);
+                printf("Reading %d\n", fd);
+                bzero(buf, BUFSIZE);
+                int n = recv(fd,buf,BUFSIZE,0);
                 buf[n] = '\0';
                 printf("Message :");
                 printf("%s\n",buf);
+                break;
             }
         }
     }
