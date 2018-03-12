5e67694b750801e87b824718cf9c13e1b28957c6
Kudimagan <n1sh0k@iitkgp.ac.in>
2018-03-13 02:20:11 +0530

    ///

--- Assignment7/send_recv.cpp
+++ Assignment7/send_recv.cpp
@@ -18,14 +18,104 @@
 #include <time.h>
 #define BUFSIZE 1024 // to divide into chunks of 1 KB
 
+struct fileDetails {
+   
+   char fileName[512];
+   int fileSize;
+   int noOfDataChunks;
 
-int udp_send(){
-	/* Sends udp packets */
+};
+
+struct header{
+  
+   int sequenceNumber;
+   int chunkLength;
+
+};
+
+struct dataPacket{
+ 
+   header packetHeader;
+   char packetContents[1024];
+
+};
+
+int udp_send(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPacket fileChunk){
+	/* Sends udp packets - sending file details*/
+    int n = sendto(sockfd, (char*)(&fileChunk), sizeof(fileChunk), 0, ((sockaddr*)&serveraddr), serverlen);
+    if(n<0)return 0;
+    return 1;
 }
 
-int create_packet(){
+int udp_send(int sockfd,struct sockaddr_in serveraddr,int serverlen,fileDetails fileChunk){
+	/* Sends udp packets - sending file data in chunks*/
+    int n = sendto(sockfd, (char*)(&fileChunk), sizeof(fileChunk), 0, ((sockaddr*)&serveraddr), serverlen);
+    if(n<0)return 0;
+    return 1;
+}
+
+int udp_send(int sockfd,struct sockaddr_in serveraddr,int serverlen,int ack){
+	/* Sends udp packets - for ack */
+    int n = sendto(sockfd, (char*)(&ack), sizeof(ack), 0, ((sockaddr*)&serveraddr), serverlen);
+    if(n<0)return 0;
+    return 1;
+}
+
+int create_file_details_packet(int sockfd,struct sockaddr_in serveraddr,int serverlen,char fileName[])
+{
+
+    struct stat inputFileInfo;
+    // Get filesize to calculate total number of fragments and total number of fragment digits
+    stat(fileName, &inputFileInfo);
+    int fileSize = inputFileInfo.st_size;
+    int noOfChunks = fileSize%1024 == 0 ? fileSize/1024 : (fileSize/1024+1);
+    fileDetails f;
+    strcpy(f.fileName,fileName);
+    f.fileSize = fileSize;
+    f.noOfDataChunks = noOfChunks;
+    return udp_send(sockfd,serveraddr,serverlen,f);
+}
+
+int create_data_packet(int sockfd,struct sockaddr_in serveraddr,int serverlen,char* data,int seqNo){
 	/* creates packets 
 	calls udp_send */
+	dataPacket d;
+	strcpy(d.packetContents,data);
+	d.packetHeader.sequenceNumber = seqNo;
+	d.packetHeader.chunkLength = strlen(d.packetContents);
+	return udp_send(sockfd,serveraddr,serverlen,d);
+
+}
+
+int udp_receive(int sockfd,struct sockaddr_in clientaddr,int clientlen,dataPacket fileChunk){
+	/* Sends udp packets - sending file details*/
+    int n = recvfrom(sockfd, (char*)&fileChunk, sizeof(fileChunk), 0,(struct sockaddr *) &clientaddr, (socklen_t*)&clientlen);
+    if(n<0)return 0;
+    return 1;
+}
+
+int udp_receive(int sockfd,struct sockaddr_in clientaddr,int clientlen,fileDetails fileChunk){
+	/* Sends udp packets - sending file data in chunks*/
+    int n = recvfrom(sockfd, (char*)(&fileChunk), sizeof(fileChunk), 0, ((sockaddr*)&clientaddr), (socklen_t*)&clientlen);
+    if(n<0)return 0;
+    return 1;
+}
+
+int udp_receive(int sockfd,struct sockaddr_in clientaddr,int clientlen,int ack){
+	/* Sends udp packets - for ack */
+    int n = recvfrom(sockfd, (char*)(&ack), sizeof(ack), 0, ((sockaddr*)&clientaddr), (socklen_t*)&clientlen);
+    if(n<0)return 0;
+    return 1;
+}
+
+int parse_packets(){
+	/* Parses received packets, separating them as ack or data packets
+	calls update window */
+}
+
+int update_window(){
+	/* updates window size on receiving the acks
+	calls rate control */
 }
 
 int rate_control(){
@@ -38,23 +128,14 @@ int rate_control(){
 int sendbuffer_handle(){
 	/* maintains queue of sender packets
 	calls rate control */
+
 }
 
-int update_window(){
-	/* updates window size on receiving the acks
-	calls rate control */
-}
 
-int parse_packets(){
-	/* Parses received packets, separating them as ack or data packets
-	calls update window */
+int appSend(){
+	/* sends data directly to sender buffer 
+	calls sendbuffer_handle */
 }
-
-int udp_receive(){
-	/* Receives udp packets
-	calls parse packets */ 
-}
-
 
 int send_ack(){
 	/* construct ack */
@@ -66,10 +147,6 @@ int recvbuffer_handle(){
 	/* calls send_ack() */
 }
 
-int appSend(){
-	/* sends data directly to sender buffer 
-	calls sendbuffer_handle */
-}
 
 int appRecv(){
 	/* calls recvbuffer_handle and gets data from there
@@ -82,5 +159,6 @@ int appRecv(){
 
 
 int main(){
-
+  
+   return 0;
 }
