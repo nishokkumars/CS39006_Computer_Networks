2a9ecfc6fdb9fa2c52acedcbda45b4fd892944c3
SubhamRajgaria <subham.rajgaria@gmail.com>
2018-03-13 21:46:51 +0530

    Made some prototypes

--- Assignment7/send_recv.cpp
+++ Assignment7/send_recv.cpp
@@ -16,20 +16,30 @@
 #include <poll.h>
 #include <stdbool.h>
 #include <time.h>
-#define BUFSIZE 1024 // to divide into chunks of 1 KB
+#include <bits/stdc++.h>
+#include <fcntl.h>
 
-struct fileDetails {
-   
-   char fileName[512];
-   int fileSize;
-   int noOfDataChunks;
+#define MSS 1024 // to divide into chunks of 1 KB
+#define SENDER_BUFFER 1024*1024 // Buffer size
+#define RECV_BUFFER 1024*1024 // Receiver buffer size
 
-};
+using namespace std;
+int ssthresh;
+int recv_window_free;
+int slowStart;
+int lastRecvd;
+int expectedRecvd;
+int cwnd;
 
 struct header{
   
    int sequenceNumber;
    int chunkLength;
+   int recv_window_left;
+   bool operator==(header& rhs)const{
+
+   	  return sequenceNumber == rhs.sequenceNumber && chunkLength == rhs.chunkLength && recv_window_left == rhs.recv_window_left;
+   }
 
 };
 
@@ -37,44 +47,27 @@ struct dataPacket{
  
    header packetHeader;
    char packetContents[1024];
+   bool operator==(dataPacket& rhs)const{
+
+         return packetHeader == rhs.packetHeader &&  ( strcmp(packetContents,rhs.packetContents) == 0 ) ;
+   }
 
 };
 
-int udp_send(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPacket fileChunk){
-	/* Sends udp packets - sending file details*/
-    int n = sendto(sockfd, (char*)(&fileChunk), sizeof(fileChunk), 0, ((sockaddr*)&serveraddr), serverlen);
-    if(n<0)return 0;
-    return 1;
-}
 
-int udp_send(int sockfd,struct sockaddr_in serveraddr,int serverlen,fileDetails fileChunk){
-	/* Sends udp packets - sending file data in chunks*/
-    int n = sendto(sockfd, (char*)(&fileChunk), sizeof(fileChunk), 0, ((sockaddr*)&serveraddr), serverlen);
-    if(n<0)return 0;
-    return 1;
-}
+vector< dataPacket > recvBuffer;
 
-int udp_send(int sockfd,struct sockaddr_in serveraddr,int serverlen,int ack){
-	/* Sends udp packets - for ack */
-    int n = sendto(sockfd, (char*)(&ack), sizeof(ack), 0, ((sockaddr*)&serveraddr), serverlen);
-    if(n<0)return 0;
-    return 1;
-}
-
-int create_file_details_packet(int sockfd,struct sockaddr_in serveraddr,int serverlen,char fileName[])
+bool compare(const dataPacket& a,const dataPacket& b)
 {
-
-    struct stat inputFileInfo;
-    // Get filesize to calculate total number of fragments and total number of fragment digits
-    stat(fileName, &inputFileInfo);
-    int fileSize = inputFileInfo.st_size;
-    int noOfChunks = fileSize%1024 == 0 ? fileSize/1024 : (fileSize/1024+1);
-    fileDetails f;
-    strcpy(f.fileName,fileName);
-    f.fileSize = fileSize;
-    f.noOfDataChunks = noOfChunks;
-    return udp_send(sockfd,serveraddr,serverlen,f);
+	return a.packetHeader.sequenceNumber < b.packetHeader.sequenceNumber;
 }
+int udp_send(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPacket fileChunk){
+	/* Sends udp packets - sending file details */
+    int n = sendto(sockfd, (char*)(&fileChunk), sizeof(fileChunk), 0, ((sockaddr*)&serveraddr), serverlen);
+    if(n<0)return 0;
+    return 1;
+}
+
 
 int create_data_packet(int sockfd,struct sockaddr_in serveraddr,int serverlen,char* data,int seqNo){
 	/* creates packets 
@@ -83,7 +76,82 @@ int create_data_packet(int sockfd,struct sockaddr_in serveraddr,int serverlen,ch
 	strcpy(d.packetContents,data);
 	d.packetHeader.sequenceNumber = seqNo;
 	d.packetHeader.chunkLength = strlen(d.packetContents);
+	if(strcmp(data,"ACK")==0)
+		d.packetHeader.recv_window_left = recv_window_free;
+	else
+		d.packetHeader.recv_window_left = -9999;
 	return udp_send(sockfd,serveraddr,serverlen,d);
+
+}
+
+int appRecv(dataPacket d){
+	/* calls recvbuffer_handle and gets data from there
+	is blocked if no data in buffer */
+	int fd = creat("ReceivedData.txt",0666);
+
+
+}
+
+int send_ack(int sockfd,struct sockaddr_in serveraddr,int serverlen, int seqNo){
+	/* construct ack */
+	/* calls udp_send */
+	char *ack = (char *)"ACK";
+	create_data_packet(sockfd, serveraddr, serverlen,ack, seqNo);
+
+}
+
+int recvbuffer_handle(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPacket d){
+	/* handles receiver buffer */
+	/* calls send_ack() */
+	lastRecvd = d.packetHeader.sequenceNumber;
+	if(lastRecvd==expectedRecvd){
+		int counter = expectedRecvd;
+		vector<dataPacket>::iterator ptr;
+		for(ptr=recvBuffer.begin();ptr<recvBuffer.end();ptr++)
+		{
+			 sort(recvBuffer.begin(),recvBuffer.end(),compare);
+             recvBuffer.erase(unique(recvBuffer.begin(), recvBuffer.end()), recvBuffer.end());
+			 if(ptr->packetHeader.sequenceNumber == counter)
+			 {
+			 	 send_ack(sockfd,serveraddr,serverlen,counter);
+			 	 counter++;
+			 	 appRecv(*ptr);
+			 	 recvBuffer.erase(recvBuffer.begin());
+			 }
+			 else break;
+		} 
+		
+	}
+	else if(lastRecvd > expectedRecvd){
+
+        recvBuffer.push_back(d);
+		send_ack(sockfd,serveraddr,serverlen, expectedRecvd);
+        sort(recvBuffer.begin(),recvBuffer.end(),compare);
+        recvBuffer.erase(unique(recvBuffer.begin(), recvBuffer.end()), recvBuffer.end());
+		recv_window_free = recv_window_free-strlen(d.packetContents);
+	}
+}
+
+int update_window(dataPacket d){
+	/* updates window size on receiving the acks
+	calls rate control */
+	if(slowStart)
+		cwnd = cwnd+MSS;
+	else
+		cwnd = cwnd+(MSS*MSS)/cwnd;
+
+}
+
+
+int parse_packets(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPacket d){
+	/* Parses received packets, separating them as ack or data packets
+	calls update window */
+	if(strcpy(d.packetContents,"ACK")==0){
+		update_window(d);
+	}
+	else{
+		recvbuffer_handle(sockfd, serveraddr, serverlen, d);
+	}
 
 }
 
@@ -91,31 +159,7 @@ int udp_receive(int sockfd,struct sockaddr_in clientaddr,int clientlen,dataPacke
 	/* Sends udp packets - sending file details*/
     int n = recvfrom(sockfd, (char*)&fileChunk, sizeof(fileChunk), 0,(struct sockaddr *) &clientaddr, (socklen_t*)&clientlen);
     if(n<0)return 0;
-    return 1;
-}
-
-int udp_receive(int sockfd,struct sockaddr_in clientaddr,int clientlen,fileDetails fileChunk){
-	/* Sends udp packets - sending file data in chunks*/
-    int n = recvfrom(sockfd, (char*)(&fileChunk), sizeof(fileChunk), 0, ((sockaddr*)&clientaddr), (socklen_t*)&clientlen);
-    if(n<0)return 0;
-    return 1;
-}
-
-int udp_receive(int sockfd,struct sockaddr_in clientaddr,int clientlen,int ack){
-	/* Sends udp packets - for ack */
-    int n = recvfrom(sockfd, (char*)(&ack), sizeof(ack), 0, ((sockaddr*)&clientaddr), (socklen_t*)&clientlen);
-    if(n<0)return 0;
-    return 1;
-}
-
-int parse_packets(){
-	/* Parses received packets, separating them as ack or data packets
-	calls update window */
-}
-
-int update_window(){
-	/* updates window size on receiving the acks
-	calls rate control */
+    return parse_packets(sockfd, clientaddr, clientlen, fileChunk);
 }
 
 int rate_control(){
@@ -128,6 +172,38 @@ int rate_control(){
 int sendbuffer_handle(){
 	/* maintains queue of sender packets
 	calls rate control */
+    /*while(nextseqnum<base+N){
+         	
+         	n = sendto(sockfd, (char*)(&filePackets[nextseqnum]), sizeof(fileChunk), 0, &serveraddr, serverlen);
+            if(base == nextseqnum){
+
+            	//start timer
+            }
+            nextseqnum++;
+         }
+         int ack;
+
+         n = recvfrom(sockfd,(char*)&ack,sizeof(int),0,&serveraddr,&serverlen);
+         if (n <= 0) {
+            
+            //start timer
+         	int temp;
+         	for(temp=base;temp<nextseqnum;temp++)
+         	{
+         		n = sendto(sockfd,(char*)(&filePackets[temp]),sizeof(fileChunk),0,&serveraddr,serverlen);
+         	}
+         }
+         printf("Message from server: ACK%d\n",ack);
+         base = ack + 1;
+         N += ack-prevReceived;
+         prevReceived = ack;
+         if(base == nextseqnum )
+         {
+            	 N*=2;//stop timer
+         }else{
+            	 
+            	 N/=2;//start timer
+         }*/
 
 }
 
@@ -137,21 +213,8 @@ int appSend(){
 	calls sendbuffer_handle */
 }
 
-int send_ack(){
-	/* construct ack */
-	/* calls udp_send */
-}
-
-int recvbuffer_handle(){
-	/* handles receiver buffer */
-	/* calls send_ack() */
-}
 
 
-int appRecv(){
-	/* calls recvbuffer_handle and gets data from there
-	is blocked if no data in buffer */
-}
 
 
 
