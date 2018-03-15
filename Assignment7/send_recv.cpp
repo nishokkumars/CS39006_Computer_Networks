802dfa14a71f55c93a4bfccec936ef5aa49888a6
SubhamRajgaria <subham.rajgaria@gmail.com>
2018-03-15 10:22:29 +0530

    Interim version

--- Assignment7/send_recv.cpp
+++ Assignment7/send_recv.cpp
@@ -22,6 +22,7 @@
 #define MSS 1024 // to divide into chunks of 1 KB
 #define SENDER_BUFFER 1024*1024 // Buffer size
 #define RECV_BUFFER 1024*1024 // Receiver buffer size
+#define TIMEOUT_VAL 1 
 
 using namespace std;
 int ssthresh;
@@ -30,6 +31,13 @@ int slowStart;
 int lastRecvd;
 int expectedRecvd;
 int cwnd;
+int base;
+int nextseqnum;
+int tempp;
+int ack;
+int prevReceived;
+time_t startTime,endTime;
+map< int, int > dupAckCount;
 
 struct header{
   
@@ -104,6 +112,7 @@ int recvbuffer_handle(int sockfd,struct sockaddr_in serveraddr,int serverlen,cha
 
 		int counter = expectedRecvd;
 		vector< pair< int , string > >::iterator ptr;
+		bool flag= false;
 		for(ptr=recvBuffer.begin();ptr<recvBuffer.end();ptr++)
 		{
 			 sort(recvBuffer.begin(),recvBuffer.end());
@@ -112,7 +121,7 @@ int recvbuffer_handle(int sockfd,struct sockaddr_in serveraddr,int serverlen,cha
 			 {
 			 	 send_ack(sockfd,serveraddr,serverlen,counter);
 			 	 counter++;
-			 	 if(strlen(receiverBuffer)+(*ptr).second.length()>RECV_BUFFER)break;
+			 	 if(strlen(receiverBuffer)+(*ptr).second.length()>RECV_BUFFER && flag == false){flag=1;continue;}
 			 	 char temp[MSS+5];
 			 	 strcpy(temp,(*ptr).second.c_str());
 			 	 strcat(receiverBuffer,temp);
@@ -135,13 +144,117 @@ int recvbuffer_handle(int sockfd,struct sockaddr_in serveraddr,int serverlen,cha
 	return 0;
 }
 
-int update_window(dataPacket d){
+int rate_control(int sockfd,struct sockaddr_in serveraddr,int serverlen){
+	/* handles flow and congestion control
+	calls create packet */ 
+	/* If timeout occurs make cwnd = 1mss and half the ssthresh 
+	if triple ack then make ssthresh half and start from there */
+    int N = cwnd;
+    int n;
+    int temp2 = tempp;
+    cout<<"in"<<endl;
+    cout<<strlen(senderBuffer)<<endl;
+    while(base<=int((ceil(1.0*strlen(senderBuffer)/MSS))))
+    {
+    	 cout<<"infor"<<endl;
+         while(nextseqnum<base+N){
+            
+            cout<<nextseqnum<<" "<<base+N<<endl;
+         	char *to_be_sent_data = (char *)malloc(MSS*sizeof(char));
+         	int lastSeqSent = nextseqnum-1;
+         	int noOfBytesSent = lastSeqSent*MSS;
+         	cout<<strlen(senderBuffer)<<" "<<noOfBytesSent<<endl;
+         	int temp1 = min(MSS,max((int)strlen(senderBuffer)-noOfBytesSent,0));
+         	cout<<"tempy"<<endl;
+         	cout<<tempp<<" "<<temp1<<endl;
+         	strncpy(to_be_sent_data,senderBuffer+tempp,temp1);
+         	cout<<"hi"<<endl;
+         	n = create_data_packet(sockfd, serveraddr, serverlen,to_be_sent_data,noOfBytesSent+temp1);
+         	cout<<"hiend"<<endl;
+            if(base == nextseqnum){
+
+            	//start timer
+            	time(&startTime);
+            }
+            nextseqnum++;
+            tempp+=temp1;
+         }
+    int flag = 0;
+    int tripleDupAck,timeout;
+    int ack;
+    n = recvfrom(sockfd,(char*)&ack,sizeof(int),0,(sockaddr*)&serveraddr,(socklen_t*)&serverlen);
+    cout<<ack<<endl;
+        exit(0);
+    if(n<=0)flag=1;
+    /*while(1)
+    {
+        time(&endTime);
+        if(endTime-startTime>=N*TIMEOUT_VAL)
+        {
+        	flag = 1;
+        	break;
+        }
+        else
+        	break;
+
+    }*/
+    if(flag==1){
+    	int ptr;
+    	timeout = 1;
+        char *to_be_sent_data = (char *)malloc(MSS*sizeof(char));
+        for(ptr = base; ptr < nextseqnum; ptr++){
+        	bzero(to_be_sent_data,MSS);
+	         int lastSeqSent = ptr-1;
+	         int noOfBytesSent = lastSeqSent*MSS;
+	         int temp1 = min(MSS,(int)strlen(senderBuffer)-noOfBytesSent);
+	         strncpy(to_be_sent_data,senderBuffer+temp2,temp1);
+        	n = create_data_packet(sockfd, serveraddr, serverlen,to_be_sent_data,noOfBytesSent+temp1);
+        	temp2+=temp1;
+        }
+
+
+    }
+    //receive signal from window_update
+    //update window
+    if(prevReceived == ack)dupAckCount[prevReceived]++;
+    if(dupAckCount[prevReceived] == 3)tripleDupAck = 1;
+    prevReceived = ack;
+    if( base == nextseqnum )
+    {      
+        ;   
+    
+    }else{
+         
+        if(timeout){
+
+		ssthresh = ssthresh/2;
+		cwnd = MSS;
+		slowStart = 1;
+	
+	    }
+	    else 	if(tripleDupAck)
+	   {
+		 ssthresh/=2;
+		 cwnd = ssthresh;
+		 slowStart = 0;
+	
+	   }
+    }
+    }
+
+}
+
+int update_window(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPacket d){
 	/* updates window size on receiving the acks
 	calls rate control */
 	if(slowStart)
 		cwnd = cwnd+MSS;
 	else
 		cwnd = cwnd+(MSS*MSS)/cwnd;
+	//get lock
+    ack = d.packetHeader.sequenceNumber;
+	//unlock
+	rate_control(sockfd,serveraddr, serverlen);
 	return cwnd;
 }
 
@@ -150,7 +263,7 @@ int parse_packets(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPac
 	/* Parses received packets, separating them as ack or data packets
 	calls update window */
 	if(strcpy(d.packetContents,"ACK")==0){
-		update_window(d);
+		update_window(sockfd,serveraddr, serverlen,d);
 	}
 	else{
 		recvbuffer_handle(sockfd, serveraddr, serverlen,d.packetContents,d.packetHeader.sequenceNumber);
@@ -165,38 +278,112 @@ int udp_receive(int sockfd,struct sockaddr_in clientaddr,int clientlen,dataPacke
     return parse_packets(sockfd, clientaddr, clientlen, fileChunk);
 }
 
-int rate_control(){
-	/* handles flow and congestion control
-	calls create packet */ 
-	/* If timeout occurs make cwnd = 1mss and half the ssthresh 
-	if triple ack then make ssthresh half and start from there */
 
-}
 
 int sendbuffer_handle(char* buffer){
 	/* maintains queue of sender packets
 	calls rate control */
 	int i = strlen(senderBuffer);
 	int k = 0;
-	if(strlen(senderBuffer)>=SENDER_BUFFER)return -1;
+	if(strlen(senderBuffer)>=SENDER_BUFFER)return 0;
 	while(i<SENDER_BUFFER && k<strlen(buffer))
 	{
 		senderBuffer[i++] = buffer[k++];
 	}
 	senderBuffer[i] = '\0';
-	return 0;
+	return k;
 }
 
 
-int appSend(){
+int appSend(char *filename){
 	
 	/* sends data directly to sender buffer 
 	calls sendbuffer_handle */
+    int sockfd, portno, n;
+    int serverlen;
+    struct sockaddr_in serveraddr;
+    struct addrinfo *my_info,hints;
+    struct hostent *server;
+    char *hostname;
+    char *fileName;
+    int BUFSIZE = 1024;
+    char buf[BUFSIZE];
 
+    /* check command line arguments */
+    hostname = "127.0.0.1";
+    portno = 8080;
+    struct timeval timeout = {1,0};
+    /* socket: create the socket */
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    setsockopt(sockfd,SOL_SOCKET,SO_RCVTIMEO,(char*)&timeout,sizeof(timeout));
+    if (sockfd < 0) 
+        perror("ERROR opening socket");
+
+    /* gethostbyname: get the server's DNS entry */
+    server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+        exit(0);
+    }
+   
+    memset (&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(NULL, filename , &hints, &my_info);
+
+    /* build the server's Internet address */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,(char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    serveraddr.sin_port = htons(portno);
+    /* get a message from the user */
+    bzero(buf, BUFSIZE);
+    serverlen = sizeof(serveraddr);
+	FILE *fp = fopen(filename,"rb");
+	if (fp == NULL) {
+       printf("File does not exist \n");
+       return 1;
+    }
+    struct stat inputFileInfo;
+    // Get filesize to calculate total number of fragments and total number of fragment digits
+    stat(filename, &inputFileInfo);
+    int fileSize = inputFileInfo.st_size;
+    int noOfChunks = fileSize%1024 == 0 ? fileSize/1024 : (fileSize/1024+1);
+    char *buffer= (char *)malloc((MSS+1)*sizeof(char));
+    bool endOfFile = feof(fp);
+    int k,l;
+    for(l=0;l<noOfChunks;l++){
+    	for(k=0;k<MSS && !endOfFile;k++){
+    		char nextChar = getc(fp);
+    		if(feof(fp)){
+    			endOfFile = true;
+    			k--;
+    		}
+    		else{
+    			buffer[k] = nextChar;
+    		}
+    	}
+    	buffer[k]='\0';
+    	sendbuffer_handle(buffer);
+    	rate_control(sockfd,serveraddr,serverlen);
+    }
 
 }
 
 int main(){
+
+	char *filename = (char *)malloc(1000*sizeof(char));
+	cout << "Enter filename" << endl;
+	cin >> filename ;
+	cwnd = 1;
+	base = 1;
+	expectedRecvd = 1;
+	nextseqnum = 1;
+	lastRecvd = 0;
+	ssthresh = 340*1024;
+	appSend(filename);
   
    return 0;
 }
