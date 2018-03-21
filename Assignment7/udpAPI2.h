97611e76504233459373204c5edac3aa497396b3
Nishok Kumar Sundaressan <n1sh0k@iitkgp.ac.in>
2018-03-21 19:46:38 +0530

    appsend()

--- Assignment7/udpAPI2.h
+++ Assignment7/udpAPI2.h
@@ -40,6 +40,7 @@ int prevReceived;
 int ack;
 bool ackArrived;
 time_t startTime,endTime;
+int senderBufferFree;
 map< int, int > dupAckCount;
 
 pthread_mutex_t slock;// Sender lock
@@ -119,17 +120,21 @@ int create_data_packet(int sockfd,struct sockaddr_in serveraddr,int serverlen,ch
 
 }
 
-int appRecv(char *buffer){
+int appRecv(int sockfd,struct sockaddr_in serveraddr,int serverlen,char *buffer){
 	/* calls recvbuffer_handle and gets data from there
 	is blocked if no data in buffer */
-	//int fd = creat("ReceivedData.txt",0666);
-  //char *buffer = (char *)malloc(sizeof(char)*RECV_BUFFER);
-  if(recv_window_free< RECV_BUFFER){
-    for(int i=0;i<RECV_BUFFER-recv_window_free;i++){
-      buffer[i]=receiverBuffer[i];
-    }
-  }
-
+    udp_recv_str temp_recv;
+    sock_det.sockfd = sockfd;
+    sock_det.serveraddr = serveraddr;
+    sock_det.serverlen = serverlen;
+    temp_recv.sock = sock_det;
+    recv_window_free = RECV_BUFFER;
+	pthread_t udp_recv_thread;
+    pthread_create(&udp_recv_thread, NULL, udp_receive,&temp_recv );
+    pthread_join(udp_recv_thread,NULL);
+    pthread_mutex_lock(&recv_buffer_lock);
+    strcpy(buffer,receiverBuffer);
+    pthread_mutex_unlock(&recv_buffer_lock);
 
 }
 
@@ -145,29 +150,35 @@ int recvbuffer_handle(int sockfd,struct sockaddr_in serveraddr,int serverlen,cha
 	/* handles receiver buffer */
 	/* calls send_ack() */
     pthread_mutex_lock(&recv_buffer_lock);
+    char tempArr[RECV_BUFFER];
+    if(strcmp(buffer,"finish")==0)
+    {
+    	send_ack(sockfd,serveraddr,serverlen,-1);
+    }
 	if(lastRecvd==expectedRecvd){
         
 		int counter = expectedRecvd;
 		vector< pair< int , string > >::iterator ptr;
 		bool flag= false;
 		recvBuffer.push_back(make_pair(lastRecvd,buffer));
+	    sort(recvBuffer.begin(),recvBuffer.end());
 		for(ptr=recvBuffer.begin();ptr<recvBuffer.end();ptr++)
 		{
-			 sort(recvBuffer.begin(),recvBuffer.end());
              recvBuffer.erase(unique(recvBuffer.begin(), recvBuffer.end()), recvBuffer.end());
 			 if((*ptr).first == counter)
 			 {
-			 	 send_ack(sockfd,serveraddr,serverlen,counter);
+			 	 send_ack(sockfd,serveraddr,serverlen,expectedRecvd);
+			 	 expectedRecvd++;
 			 	 counter++;
-			 	 if(strlen(receiverBuffer)+(*ptr).second.length()>RECV_BUFFER && flag == false){flag=1;continue;}
 			 	 char temp[MSS+5];
 			 	 strcpy(temp,(*ptr).second.c_str());
-			 	 strcat(receiverBuffer,temp);
+			 	 strcat(tempArr,temp);
 			 	 recvBuffer.erase(recvBuffer.begin());
 			 }
 			 else break;
-		} 
-		expectedRecvd = counter;
+		}
+		strcat(receiverBuffer,tempArr); 
+        
 	
 	}
 	else if(lastRecvd > expectedRecvd){
@@ -176,6 +187,9 @@ int recvbuffer_handle(int sockfd,struct sockaddr_in serveraddr,int serverlen,cha
     sort(recvBuffer.begin(),recvBuffer.end());
     recvBuffer.erase(unique(recvBuffer.begin(), recvBuffer.end()), recvBuffer.end());
 		recv_window_free = recv_window_free-strlen(buffer);
+	}else if(lastRecvd < expectedRecvd)
+	{
+		send_ack(sockfd,serveraddr,serverlen,expectedRecvd);
 	}
     pthread_mutex_unlock(&recv_buffer_lock);
 	return 0;
@@ -289,6 +303,33 @@ void * rate_control(void * param){
     }
     pthread_mutex_unlock(&other_var_lock);
   }
+  int p = 0;
+  while(1)
+  {
+     n = create_data_packet(sockfd, serveraddr, serverlen,(char*)"finish",-1);
+     time(&startTime);
+     int flag = 0;
+     while(1)
+     {
+        pthread_mutex_lock(&other_var_lock);
+        if(ackArrived == true){temp3 = 1;ackArrived = false;}
+        pthread_mutex_unlock(&other_var_lock);
+        if(temp3==1)break;
+        time(&endTime);
+        if(endTime-startTime>=N*TIMEOUT_VAL)
+        {
+        	flag = 1;
+        	p++;
+        	break; 
+        }
+
+     }
+     if(p==3)break;
+     pthread_mutex_lock(&other_var_lock);
+     base = ack+1;
+     if(ack == -1){pthread_mutex_unlock(&other_var_lock);break;}
+     pthread_mutex_unlock(&other_var_lock);
+  }
 }
 
 int update_window(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPacket d){
@@ -307,14 +348,17 @@ int update_window(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPac
   
   }
 	//get lock
+
   ack = d.packetHeader.sequenceNumber;
   ackArrived = 1;
   recv_window_free = d.packetHeader.recv_window_left;
+  if(d.packetHeader.sequenceNumber == -1)
+  return -1;
   int temp = cwnd;
   pthread_mutex_unlock(&other_var_lock);
 	//unlock
 	//rate_control(sockfd,serveraddr, serverlen);
-	return temp;
+  return 0;
 }
 
 
@@ -322,13 +366,12 @@ int parse_packets(int sockfd,struct sockaddr_in serveraddr,int serverlen,dataPac
 	/* Parses received packets, separating them as ack or data packets
 	calls update window */
 	if(strcmp(d.packetContents,"ACK")==0){
-		update_window(sockfd,serveraddr, serverlen,d);
+		return update_window(sockfd,serveraddr, serverlen,d);
 	}
 	else{
 
-		recvbuffer_handle(sockfd, serveraddr, serverlen,d.packetContents,d.packetHeader.sequenceNumber);
+		return recvbuffer_handle(sockfd, serveraddr, serverlen,d.packetContents,d.packetHeader.sequenceNumber);
 	}
-
 }
 
 void * udp_receive(void *param){
@@ -343,62 +386,50 @@ void * udp_receive(void *param){
     {
       int n = recvfrom(sockfd, (char*)&fileChunk, sizeof(fileChunk), 0,(struct sockaddr *) &clientaddr, (socklen_t*)&clientlen);
       if(n>=0)
-      parse_packets(sockfd, clientaddr, clientlen, fileChunk);
+      if(parse_packets(sockfd, clientaddr, clientlen, fileChunk) < 0 )
+      	break;
+    
     }
+
 }
 
-
-
-int sendbuffer_handle(char* buffer){
+int sendbuffer_handle(int sockfd,sockaddr_in serveraddr,int serverlen,char* buffer){
 	/* maintains queue of sender packets
 	calls rate control */
-    pthread_mutex_lock(&send_buffer_lock);
-    int i = strlen(senderBuffer);
+
+    int i = 0;
 	int k = 0;
-	if(strlen(senderBuffer)>=SENDER_BUFFER)return 0;
+	if(strlen(buffer)>=SENDER_BUFFER)return -1;
 
     /* 
     Used lock Here
      */
-	while(i<SENDER_BUFFER && k<strlen(buffer))
+	while(k<strlen(buffer))
 	{
-		senderBuffer[i++] = buffer[k++];
+		senderBuffer[k] = buffer[k];
+		k++;
 	}
-	senderBuffer[i] = '\0';
-    pthread_mutex_unlock(&send_buffer_lock);
-	return k;
+	senderBuffer[k] = '\0';
+    socket_details sock_det;
+    udp_recv_str temp_recv;
+    sock_det.sockfd = sockfd;
+    sock_det.serveraddr = serveraddr;
+    sock_det.serverlen = serverlen;
+    temp_recv.sock = sock_det;
+	pthread_t udp_recv_thread, congestion_control_thread;
+    pthread_create(&udp_recv_thread, NULL, udp_receive,&temp_recv );
+    pthread_create(&congestion_control_thread, NULL, rate_control, &sock_det);
+    pthread_join(congestion_control_thread,NULL);
+    pthread_join(udp_recv_thread,NULL);
+    return 0;
 }
 
 
-int appSend(int sockfd,sockaddr_in serveraddr, int serverlen, char * buffer2){
-
-	 sendbuffer_handle(buffer);
-	 /*FILE *fp = fopen(buffer2,"rb");
-	 if (fp == NULL) {
-       printf("File does not exist \n");
-       return 1;
-    }
-    struct stat inputFileInfo;
-    // Get filesize to calculate total number of fragments and total number of fragment digits
-    stat(buffer2, &inputFileInfo);
-    int fileSize = inputFileInfo.st_size;
-    int noOfChunks = fileSize%1024 == 0 ? fileSize/1024 : (fileSize/1024+1);
-    char *buffer= (char *)malloc((MSS+1)*sizeof(char));
-    bool endOfFile = feof(fp);
-    int k,l;
-    for(l=0;l<noOfChunks;l++){
-    	for(k=0;k<MSS && !endOfFile;k++){
-    		char nextChar = getc(fp);
-    		if(feof(fp)){
-    			endOfFile = true;
-    			k--;
-    		}
-    		else{
-    			buffer[k] = nextChar;
-    		}
-    	}
-    	buffer[k]='\0';
-    	sendbuffer_handle(buffer);
-    }*/
-
+int appSend(int sockfd,sockaddr_in serveraddr, int serverlen,char * buffer2){
+{     
+          int t;
+          pthread_mutex_lock(&send_buffer_lock);
+	      t = sendbuffer_handle(sockfd,serveraddr,serverlen,temp);
+          pthread_mutex_unlock(&send_buffer_lock);
+          return t;
 }
